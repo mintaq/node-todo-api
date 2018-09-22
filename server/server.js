0fbed174186d217ff4b3dd968d76731156cd31aa
mintaq <minh.tranquang@gmail.com>
2018-09-22 23:42:09 +0700

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 
@@ -21,9 +21,16 @@ app.post('/todos', (req, res) => {
    });
 });
 
-module.exports = {app};
-
+app.get('/todos', (req, res) => {
+   Todo.find().then((todos) => {
+      res.send({ todos });
+   }, (err) => {
+      res.status(400).send(err);
+   });
+});
 
 app.listen(3000, () => {
    console.log('Started on port 3000');
 });
+
+module.exports = { app };
