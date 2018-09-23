dca2e3e639daeac56df92ffcb374657633691344
mintaq <minh.tranquang@gmail.com>
2018-09-23 10:51:02 +0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -24,10 +25,31 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
    Todo.find().then((todos) => {
       res.send({ todos });
-   }, (err) => {
-      res.status(400).send(err);
+   }, (e) => {
+      res.status(400).send(e);
    });
 });
+
+//GET /todos/123123
+app.get('/todos/:id', (req, res) => {
+   var id = req.params.id;
+
+   //Valid id using isValid
+   if (!ObjectID.isValid(id)) {
+      return res.status(404).send();
+   }
+
+   //findById
+   Todo.findById(id).then((todo) => {
+      if (!todo) {
+         return res.status(404).send();
+      }
+
+      res.send({todo});
+   }).catch((e) => {
+      res.status(400).send();
+   })
+})
 
 app.listen(3000, () => {
    console.log('Started on port 3000');
